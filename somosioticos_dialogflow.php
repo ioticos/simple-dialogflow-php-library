005e8784e0ceedd076c0afc235a5ec428a9900ba
Emmanuel Gonzalez <udsmail@gmail.com>
2020-05-12 23:43:53 +0100

    leve cambio en el mensaje de error

--- somosioticos_dialogflow.php
+++ somosioticos_dialogflow.php
@@ -256,7 +256,7 @@ function autentificacion(){
     if (!isset($_SERVER['PHP_AUTH_USER'])) {
         header('WWW-Authenticate: Basic realm="My Realm"');
         header('HTTP/1.0 401 Unauthorized');
-        echo 'Text to send if user hits Cancel button';
+        echo 'AUTENTIFICACIÓN DENEGADA';
         exit;
     }
   }
